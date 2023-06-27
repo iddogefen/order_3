497ed8256ddd940c610b75e1f508e5b1b16d7a9e
iddo gefen <iddo.gefen@gmail.com>
2023-06-27 01:44:54 -0400

    First commit

--- Functions.js
+++ Functions.js
@@ -4,7 +4,7 @@
 function save_server_data(name, data) {
   var xhr = new XMLHttpRequest();
   //xhr.addEventListener("load", onComplete);
-  xhr.open('POST', 'Tools/write_data.php'); // 'write_data.php' is the path to the php file described above.
+  xhr.open('POST', 'write_data.php'); // 'write_data.php' is the path to the php file described above.
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({
     filename: name,
